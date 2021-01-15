febd66f7e6474774edf34df43459c56f78e8764c
camicasii <camicasii@hotmail.com>
2021-01-15 13:54:23 -0400

    add commit soon

---
+++ commit_soon/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+
+ <div class="hero css-selector">
+ 
+ <section class="container">
+   <article class="flex flex-col  items-center justify-center z-10 space-y-3">
+    <img class="w-5/6 z-10" src="./assets/logo.svg" alt="logo Tronefeec" srcset="">
+    <p class="text-sm md:text-base text-center z-10
+    text-green-300 
+    font-bold
+    mx-4    
+    " >Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+   </article>
+  </section>  
+
+
+
+
+<div class="cube"></div>
+<div class="cube"></div>
+<div class="cube"></div>
+<div class="cube"></div>
+   
+ </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style>
